4763227c3c740b30bf56761685f56d8fbbfcd64f
SHM_UGH <shm.ughh@gmail.com>
2026-05-07 19:03:53 +0200

    dgfhujkl

--- app/notes/filter/layout.tsx
+++ app/notes/filter/layout.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import css from './LayoutNotes.module.css';
 
-export default function FilterLayout({
-  children,
-  sidebar,
-  modal,
-}: {
+interface FilterLayoutProps {
   children: React.ReactNode;
   sidebar: React.ReactNode;
   modal: React.ReactNode;
-}) {
+}
+
+export default function FilterLayout(props: FilterLayoutProps) {
+  const { children, sidebar, modal } = props;
+
   return (
     <div className={css.layout}>
-      <aside className={css.sidebar}>{sidebar}</aside>
+      <aside className={css.sidebar}>
+        {sidebar}
+      </aside>
       <main className={css.content}>
         {children}
-        {modal}
       </main>
+      {modal}
     </div>
   );
 }
