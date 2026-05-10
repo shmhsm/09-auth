91191519c5a101fdbc12caa08b160f162ef0cbdf
SHM_UGH <shm.ughh@gmail.com>
2026-05-10 23:28:59 +0200

    akakkaa

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,11 +1,11 @@
-import { NextResponse } from 'next/server';
-import { api } from '@/app/api/api';
+import { NextRequest, NextResponse } from 'next/server';
+import { api } from '../../api';
 import { cookies } from 'next/headers';
 import { parse } from 'cookie';
 import { isAxiosError } from 'axios';
 import { logErrorResponse } from '../../_utils/utils';
 
-export async function POST(req: Request) {
+export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
     const apiRes = await api.post('auth/login', body);
